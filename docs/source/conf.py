6741ea78609a638e660aa32d12d3b8047919cbc3
vadimsteckler1 <97836401+vadimsteckler1@users.noreply.github.com>
2022-11-19 13:48:52 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'GO23 Wallet'
+copyright = '2022, Coins.ph'
+author = 'Product Group'
 
 release = '0.1'
 version = '0.1.0'
